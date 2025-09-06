2f6c724325a335b061ac3c3359e16d6d6f97135d
BitMyTongue <ango24@myseneca.ca>
2025-09-05 23:37:05 -0400

    First Assignment

---
+++ Week1/FirstOpenTK/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+namespace FirstOpenTK
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            using (Game game = new Game())
+            {
+                game.Run();
+            } 
+        }
+    }
+}
